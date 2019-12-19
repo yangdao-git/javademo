a2dd7594f2cc13f647970a90bf79deec25f4a844
yangdao <1278578133@qq.com>
2019-12-19 10:54:10 +0800

    修改文件

--- src/Demo.java
+++ src/Demo.java
@@ -2,8 +2,8 @@ public class Demo {
     public static void main(String[] args) {
         init();
     }
-
     private static void init() {
+        System.out.println("haha ...");
         Student student = new Student();
         System.out.println(student.getName());
     }
