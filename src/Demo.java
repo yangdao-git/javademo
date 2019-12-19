93b0ccae5d18eb62437b5a7190980d5369d35cf2
yangdao <1278578133@qq.com>
2019-12-19 10:38:29 +0800

    xgwj

---
+++ src/Demo.java
@@ -0,0 +1,12 @@
+public class Demo {
+    public static void main(String[] args) {
+        init();
+    }
+
+    private static void init() {
+
+
+        Student student = new Student();
+        System.out.println(student.getName());
+    }
+}
